822ddfd5debf359f2c56e3dfb1e051b266ee2489
slode <rob.m@myplanet.com>
2017-02-06 10:37:42 -0500

    implement basic server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const express = require('express')
+const WebSocketServer = require('ws').Server
+const url = require('url')
+
+const app = express()
+
+const server = app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
+
+const wss = new WebSocketServer({
+  server: server,
+  path: '/ws'
+})
+
+wss.on('connection', ws => {
+  console.log('client connected')
+
+  const location = url.parse(ws.upgradeReq.url, true)
+
+  ws.on('message', msg => {
+    console.log(`received: ${ msg }`)
+  })
+
+  ws.send('something')
+})
